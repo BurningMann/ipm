44deb6abfd09c581b3c55ea7ac13aa74a4a74105
BurningMann <sannnnna@bk.ru>
2021-12-08 19:30:59 +0300

    js fix

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -7,7 +7,7 @@ window.onload = function(){
     $(clients).addClass('visible')
   }else{
     $(window).scroll(function(){
-      if(this.pageYOffset + (window.innerHeight / 2) >= $(clients).offset().top ){
+      if(clients &&  this.pageYOffset + (window.innerHeight / 2) >= $(clients).offset().top){
         $(clients).addClass('visible')
       }
     })
@@ -21,7 +21,7 @@ window.onload = function(){
     });
   }else{
     $(window).scroll(function(){
-      if(this.pageYOffset + (window.innerHeight / 1.5) >= $(about).offset().top && !$(about).find(".number").hasClass('number--black')){
+      if(about && this.pageYOffset + (window.innerHeight / 1.5) >= $(about).offset().top && !$(about).find(".number").hasClass('number--black')){
         $(about).find(".number").addClass('number--black')
         $(about).find(".number").delay(500).spincrement({
           thousandSeparator: "",
